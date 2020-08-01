ff6181a64c8588d7fa39522857b7bb30ef0fb64d
felix <25915239+f3l1xss@users.noreply.github.com>
2020-08-01 16:15:42 +0700

    first commit

---
+++ src/main/java/com/felixstanley/makanmoerahinvoicejob/MakanmoerahInvoiceJobApplication.java
@@ -0,0 +1,13 @@
+package com.felixstanley.makanmoerahinvoicejob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakanmoerahInvoiceJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakanmoerahInvoiceJobApplication.class, args);
+	}
+
+}
